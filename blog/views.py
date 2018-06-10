91701ce40d845fbff33a09a08effeb158e35b918
unknown <elicarlosantos_@hotmail.com>
2018-06-10 12:58:39 -0300

    Feito ajustes de urls

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+
+def post_detail(request, pk):
+    Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
+    return render (request, 'blog/post_detail.html', {'post' : post})
